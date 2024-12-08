a03cafe384156a69eb0bd8421b5fee031a5de2bb
Yehia003 <mohamedezio57@gmail.com>
2024-12-08 14:54:35 +0200

    Last Stand

--- app/src/main/java/com/yehia/secondtrail/MainActivity.kt
+++ app/src/main/java/com/yehia/secondtrail/MainActivity.kt
@@ -6,42 +6,51 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.yehia.secondtrail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var array  = mutableListOf<movie>()
 
-    lateinit var binding: ActivityMainBinding
+    private var array  = mutableListOf<movie>()
+    private  fun add(){
+        var m = movie("aot",R.drawable.aot)
+        array.add(m)
+        m = movie("avengers",R.drawable.avgend)
+        array.add(m)
+        m = movie("captain america",R.drawable.captainamerica)
+        array.add(m)
+        m = movie("Inspection",R.drawable.insepction)
+        array.add(m)
+        m = movie("Shad 2gaz2",R.drawable.shadagzaa)
+        array.add(m)
+        m = movie("DemonSlayer",R.drawable.demonslayer)
+        array.add(m)
+        m = movie("Wolvrine",R.drawable.wolvrine)
+        array.add(m)
+        m = movie("Tohyo drift",R.drawable.tokyodrift)
+        array.add(m)
+        m= movie("man of steel" , R.drawable.manofstell)
+        array.add(m)
+        m = movie("Spiderman IntoSpiderVerse" , R.drawable.spman)
+    }
+
+
+
+
+
+
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         add()
         val obj = MyAdapter(array)
         binding.movieRv.adapter = obj
         binding.movieRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-        binding.toolbar.setTitle("Hebesha")
+        binding.toolbar.setTitle("Sheba")
         setSupportActionBar(binding.toolbar)
     }
 
 
-    private  fun add(){
-        var m = movie("aot",R.drawable.aot)
-         array.add(m)
-         m = movie("avengers",R.drawable.avgend)
-        array.add(m)
-         m = movie("captain america",R.drawable.captainamerica)
-        array.add(m)
-         m = movie("Inspection",R.drawable.insepction)
-        array.add(m)
-         m = movie("Shad 2gaz2",R.drawable.shadagzaa)
-        array.add(m)
-         m = movie("DemonSlayer",R.drawable.demonslayer)
-        array.add(m)
-          m = movie("Wolvrine",R.drawable.wolvrine)
-          array.add(m)
-          m = movie("Tohyo drift",R.drawable.tokyodrift)
-          array.add(m)
-          m= movie("man of steel" , R.drawable.manofstell)
-          array.add(m)
-          m = movie("Spiderman IntoSpiderVerse" , R.drawable.spman)
-    }
+
 
 }
--- app/src/main/java/com/yehia/secondtrail/MainActivity2.kt
+++ app/src/main/java/com/yehia/secondtrail/MainActivity2.kt
@@ -1,11 +1,13 @@
 package com.yehia.secondtrail
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.yehia.secondtrail.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -17,9 +19,24 @@ class MainActivity2 : AppCompatActivity() {
         val e = binding2.em.text
         val p = binding2.pass.text
         binding2.btn.setOnClickListener(){
-            Toast.makeText(this,e,Toast.LENGTH_SHORT).show()
+            val esize : Int = binding2.em.text.length
+            val psize : Int = binding2.pass.text.length
+            if(esize < 8 || psize < 8  ){
+                Toast.makeText(this,"Invalid",Toast.LENGTH_SHORT).show()
+                Snackbar.make(binding2.root,"Size Must be 8 or more",Snackbar.LENGTH_SHORT).show()
+            }else{
+            Toast.makeText(this,"Valid",Toast.LENGTH_SHORT).show()
+            val intent = intent ( this , MainActivity::class.java)
+            startActivity(intent)
+                finish()
+            }
+
         }
 
 
+    }
+
+    private fun intent(mainActivity2: MainActivity2, java: Class<MainActivity>): Intent? {
+        TODO("Not yet implemented")
     }
 }
